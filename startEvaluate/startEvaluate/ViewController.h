8de43176f86764af39a589c7de70d6efb3381c1b
xuliran33 <476883317@qq.com>
2018-03-30 15:15:38 +0800

    创建评分项目

---
+++ startEvaluate/startEvaluate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  startEvaluate
+//
+//  Created by yuedao on 2018/3/30.
+//  Copyright © 2018年 Yuedao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ startEvaluate/startEvaluate/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  startEvaluate
+//
+//  Created by yuedao on 2018/3/30.
+//  Copyright © 2018年 Yuedao. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
